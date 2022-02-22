17d4bee3190fbbb3d608a6ea0fa15219f0a2fc62
Bill Matlock <bill@billbuilt.com>
2022-02-22 14:44:27 -0500

    nothing to report here

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,6 +6,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-Icon should show here: <Icon data={faHome} slot='button'
-							 class='w-5 h-5 ml-2 -mr-1'
-							 aria-hidden='true' />
+Icon should show here: <Icon data={faHome} />
