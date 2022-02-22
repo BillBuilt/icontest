ef7f7a04a50134ac6927ab229347189c21b4e6a5
Bill Matlock <bill@billbuilt.com>
2022-02-22 14:39:24 -0500

    Set up icon test to see if it runs using npm run preview

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
+<script>
+	import Icon from 'svelte-awesome'
+	import { faHome } from '@fortawesome/free-solid-svg-icons'
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+Icon should show here: <Icon data={faHome} slot='button'
+							 class='w-5 h-5 ml-2 -mr-1'
+							 aria-hidden='true' />
